2e3186e58b250a4f9796c40eadb629690ad15c79
Devon McGrath <devonmcg1213@gmail.com>
2024-05-22 18:44:49 -0400

    Completed the Sprint 7 module 1 project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Routes, Route, Link } from 'react-router-dom';
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList';
 import SavedList from './Movies/SavedList';
 
 export default function App () {
@@ -13,7 +15,9 @@ export default function App () {
         .get('http://localhost:5001/api/movies') // Study this endpoint with Postman
         .then(response => {
           // Study this response with a breakpoint or log statements
+          // console.log(response);
           // and set the response data as the 'movies' slice of state
+          setMovies(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -29,8 +33,10 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
+      <Routes>
+        <Route path='/' element={<MovieList movies={ movies }/>}/>
+        <Route path='movies/:itemID' element={<Movie />}/>
+      </Routes>
     </div>
   );
 }
